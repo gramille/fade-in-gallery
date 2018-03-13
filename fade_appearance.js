289855ca7e376657beb47bd11e42a5ca7e1a5602
Camille <camillegrenier@orange.fr>
2018-03-13 13:56:51 +0100

    Add files via upload

---
+++ fade_appearance.js
@@ -0,0 +1,41 @@
+$(function () {
+
+    /**
+     * Apparition des miniatures de looks
+     */
+    var imgLook = Array.prototype.slice.call(document.getElementsByClassName("galleryImg"));
+
+    function imgLoaded() {
+
+        this.src = this.getAttribute("data-src");
+        this.classList.add("appeared");
+    }
+
+    function appear() {
+
+        var current = imgLook.shift();
+
+        if (imgLook.length) {
+
+            setTimeout(appear, 150);
+        }
+
+        if (!current.complete) {
+
+            var loader = new Image();
+
+            loader.addEventListener("load", imgLoaded.bind(current));
+
+            loader.src = current.src;
+        } else {
+
+            imgLoaded.call(current);
+        }
+    }
+
+    if (imgLook.length) {
+
+        appear();
+    }
+
+});
